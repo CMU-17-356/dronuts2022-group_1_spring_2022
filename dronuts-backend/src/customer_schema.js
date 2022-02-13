8cda4ec1e22af6b024d16e6efe6626e114d9fc96
Max Sobkov <sobkovmax@gmail.com>
2022-02-12 20:10:32 -0500

    fixed eslint discrepancy

--- dronuts-backend/src/customer_schema.js
+++ dronuts-backend/src/customer_schema.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-  id: {type: Number, required: true},
-  first_name: String,
-  last_name: String,
-  payment_info: String,
-  default_address: String,
-  contact_info: [String],
-  username: String,
-  password: String,
+    id: {type: Number, required: true},
+    first_name: String,
+    last_name: String,
+    payment_info: String,
+    default_address: String,
+    contact_info: [String],
+    username: String,
+    password: String,
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
--- dronuts-backend/test/schema/storeItem.test.js
+++ dronuts-backend/test/schema/storeItem.test.js
@@ -3,14 +3,17 @@ const expect = require('chai').expect;
 const assert = require('chai').assert;
 
 describe('StoreItems must have an id', function() {
-  expect(function () {
-    new StoreItem().validate(function(err) {
-      expect(err).to.exist;
-    })
-  })
-})
+    expect(function() {
+        new StoreItem().validate(function(err) {
+            expect(err).to.exist;
+        });
+    });
+});
 
-describe('StoreItem should be able to be created with valid parameters', function() {
-  const storeItem = new StoreItem({id:1, name:"donut", price:24.70});
-  assert(storeItem.validate());
-})
+describe('StoreItem should be able to be created with valid parameters',
+    function() {
+        const storeItem = new StoreItem({id: 1,
+            name: 'donut',
+            price: 24.70});
+        assert(storeItem.validate());
+    });

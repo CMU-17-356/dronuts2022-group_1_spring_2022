5c40960b7d0020284126b0e8c0ad80269e109a6f
khushi02 <k9wadhwa@gmail.com>
2022-02-14 11:18:11 -0500

    initial order page

--- dronuts-frontend/src/pages/Order.tsx
+++ dronuts-frontend/src/pages/Order.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+// From the Material UI docs
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Checkout from "./Checkout";
+import "./Order.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Order Confirmation Page!
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-      Order Confirmation Page!
-    </div>
-  );
+  return <Checkout />;
 }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 
 export default App;

30cf16561dce0721b6c49c55c7b318c16642e905
khushi02 <k9wadhwa@gmail.com>
2022-02-15 10:23:36 -0500

    order summary/checkout updates

--- dronuts-frontend/src/pages/Order.tsx
+++ dronuts-frontend/src/pages/Order.tsx
@@ -1,23 +1,11 @@
 // From the Material UI docs
 
 import React from "react";
-import ReactDOM from "react-dom";
-import Checkout from "./Checkout";
+import Checkout from "../components/Checkout";
 import "./Order.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       Order Confirmation Page!
-//     </div>
-//   );
-// }
 
 function App() {
   return <Checkout />;
 }
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
 
 export default App;

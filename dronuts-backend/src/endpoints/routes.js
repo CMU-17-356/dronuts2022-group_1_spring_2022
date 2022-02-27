840acc4c04088d6ac049e571856b77e56c4dfd52
SeanEZhou <sezhou@andrew.cmu.edu>
2022-02-27 16:56:44 -0500

    added route to get all orders

--- dronuts-backend/src/endpoints/routes.js
+++ dronuts-backend/src/endpoints/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {persistOrder} = require('./order_routes');
+const {startDatabase} = require('../db/db');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,19 @@ app.use(bodyParser.json());
 app.post('/order', async (request, response) => {
     const success = await persistOrder(request.body);
     response.send(success ? 'Order Placed' : 'Validation Error');
+});
+
+app.get('/order', (request, response) => {
+    const client = startDatabase();
+    client.connect(function(err, db) {
+        if (err) throw err;
+        const dbo = db.db('dronuts');
+        dbo.collection('Order').find({}).toArray(function(err, result) {
+            if (err) throw err;
+            response.status(200).send(result);
+            db.close();
+        });
+    });
 });
 
 app.listen(port, hostname, () => {

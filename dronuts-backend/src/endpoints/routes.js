419ed5be24f02225fdff2c255ab6516cef76e649
khushi02 <k9wadhwa@gmail.com>
2022-03-01 22:09:27 -0500

    Update route.js

--- dronuts-backend/src/endpoints/routes.js
+++ dronuts-backend/src/endpoints/routes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {persistOrder, getOrders} = require('./order_routes');
-const {startDatabase} = require('../db/db');
-const {updateOrderStatus} = require('./order_status_routes');
-const {orderStatusStatus} = require('./../enums');
-const cors = require('cors')
 
 const app = express();
 const port = 3001;

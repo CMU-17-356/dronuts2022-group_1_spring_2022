0e8be05b94a0d8b340b6c349faf03cbe82679b9a
Max Sobkov <sobkovmax@gmail.com>
2022-03-01 21:59:55 -0500

    Merge fixes down the road

--- dronuts-backend/src/endpoints/routes.js
+++ dronuts-backend/src/endpoints/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {persistOrder, getOrders} = require('./order_routes');
-const {startDatabase} = require('../db/db');
 const cors = require('cors');
 
 const app = express();
